849545017a6f61c0dc6d04d6356ecf534e0c346d
erug-the-creator <mail@emilrugenstein.com>
2022-02-17 15:28:48 +0100

    made browsersync work

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass')(require('sass'));
 var prefix      = require('gulp-autoprefixer');
 var cp          = require('child_process');
 var cssnano 		= require('gulp-cssnano');
@@ -22,25 +22,15 @@ gulp.task('jekyll-build', function (done) {
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function () {
+gulp.task('jekyll-rebuild', gulp.series('jekyll-build', function (cb) {
     browserSync.reload();
-}));
-
-/**
- * Wait for jekyll-build, then launch the Server
- */
-gulp.task('browser-sync', gulp.series(('sass', 'jekyll-build'), function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
+    cb();
 }));
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
+ gulp.task('sass', function () {
     return gulp.src('assets/scss/style.scss')
         .pipe(sass({
             includePaths: ['scss'],
@@ -54,12 +44,25 @@ gulp.task('sass', function () {
 });
 
 /**
+ * Wait for jekyll-build, then launch the Server
+ */
+gulp.task('browser-sync', gulp.series('sass', 'jekyll-build', function(cb) {
+    browserSync.init({
+        server: {
+            baseDir: '_site'
+        }
+    });
+    cb();
+}));
+
+/**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-    gulp.watch(['assets/scss/*.scss', 'assets/scss/*/*.scss'], ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+gulp.task('watch', function (cb) {
+    gulp.watch(['assets/scss/*.scss', 'assets/scss/*/*.scss'], gulp.series('sass'));
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], gulp.series('jekyll-rebuild'));
+    cb();
 });
 
 /**
